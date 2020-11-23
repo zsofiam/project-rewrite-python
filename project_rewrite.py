469d27a11d8c57c58e57c5d5cfa96c6e9bd41df6
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-11-23 14:04:22 +0100

    solved

--- project_rewrite.py
+++ project_rewrite.py
@@ -1,22 +1,55 @@
 def min(x, y):
-    pass
-
+    if x <= y:
+        return x
+    else:
+        return y
+  
 
 def max(values_list):
-    pass
+    if len(values_list) == 0:
+        return 0
+    max = values_list[0]
+    for elem in values_list:
+        if elem > max:
+            max = elem
+    return max
 
 
 def len(values_list):
-    pass
+    count = 0
+    for elem in values_list:
+        count += 1
+    return count
 
 
 def multiply(x, y):
-    pass
+    result = 0
+    for i in range(y):
+        result += x
+    return result
 
 
 def pow(x, y):
-    pass
+    result = 1
+    for i in range(y):
+        result *= x
+    return result
 
 
 def divmod(x, y):
-    pass
+    quotient = 0
+    remainder = 0
+    mylist = [0, 0]
+    if y == 0:
+        return None
+    while x-y >= 0:
+        quotient += 1
+        x -= y
+    remainder = x
+    mylist[0] = quotient
+    mylist[1] = remainder
+    return mylist
+
+
+if __name__ == '__main__':
+    print(divmod(12, 5))
